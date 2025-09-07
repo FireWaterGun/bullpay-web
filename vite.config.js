21b653c65b0e2e7ea7d2dfafda61b4adb33cbf8b
ToCHe <kiattichai.rr@gmail.com>
2025-09-07 11:37:18 +0700

    update port web run 3334

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3334,
+    strictPort: true,
+    host: true,
+  },
+  preview: {
+    port: 3334,
+    strictPort: true,
+    host: true,
+  },
 })
